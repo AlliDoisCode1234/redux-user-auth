a0a3f51e9add1d367aa7959bd60ab865e6a16e60
Josiah <josiah.james.fowler@gmail.com>
2021-06-18 01:10:29 -0400

    all routes work

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <div className="header">
+            
             <Link to="/home">
                 <h1>Link to Home</h1>
             </Link>
